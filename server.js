0f8c0e9ba255962b73d1897c4a7f4ef3a24fde4c
Julien Athomas <julienathomas@Juliens-MacBook-Pro.local>
2017-06-11 02:36:50 +0200

    first

---
+++ server.js
@@ -0,0 +1,34 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler (req, res) {
+}
+
+io.on('connection', function (socket) {
+  console.log("here");
+  console.log(socket)
+  socket.emit('news', { hello: 'world' });
+  socket.on('file transfer', function (data) {
+    socket.broadcast.emit('news', { hello: 'world' })
+    console.log(data);
+  });
+
+  socket.on('begin transfer', function (data) {
+    socket.emit('create file', data)
+    socket.broadcast.emit('create file', data)
+  });
+
+  socket.on('processing transfer', function (val, size) {
+    socket.emit('write file', val, size)
+    socket.broadcast.emit('write file', val, size)
+  });
+
+  socket.on('end transfer', function (data) {
+    socket.emit('transfer finished', data)
+    socket.broadcast.emit('transfer finished', data)
+  });
+
+});
